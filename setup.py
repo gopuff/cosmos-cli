f5e96183a1a3f96e8f974f5b8c247fe5c24a6311
Ethan McCreadie <ethan.mccreadie@gopuff.com>
2018-10-31 11:30:43 -0400

    Support Python 2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cosmos-cli',
-    version='0.1',
+    version='0.2',
     url='https://github.com/gopuff/cosmos-cli',
     maintainer='Ethan McCreadie',
     maintainer_email='ethan.mccreadie@gopuff.com',
