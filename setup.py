49828ed87cbb7e1e2dff50c763c28dd6f6de3d66
Ethan McCreadie <ethan.mccreadie@gopuff.com>
2018-10-31 11:20:31 -0400

    PyPI details

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='cosmos-cli',
     version='0.1',
+    url='https://github.com/gopuff/cosmos-cli',
+    maintainer='Ethan McCreadie',
+    maintainer_email='ethan.mccreadie@gopuff.com',
     scripts=['cosmos-cli'],
     install_requires=[
         'pydocumentdb',
